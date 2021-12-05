ef708d2487836c0ceca65b261bb00460e2366593
kutsa1 <82546005+kutsa1@users.noreply.github.com>
2021-12-05 15:19:07 +0300

    updated on the bussiness layer

--- src/main/java/com/miro/dibt/business/concretes/AdminManager.java
+++ src/main/java/com/miro/dibt/business/concretes/AdminManager.java
@@ -1,8 +1,11 @@
 package com.miro.dibt.business.concretes;
 
 import com.miro.dibt.business.abstracts.IAdminService;
+import com.miro.dibt.business.tools.Messagess;
 import com.miro.dibt.core.utilities.DataResult;
 import com.miro.dibt.core.utilities.IResult;
+import com.miro.dibt.core.utilities.SuccesDataResult;
+import com.miro.dibt.core.utilities.SuccessResult;
 import com.miro.dibt.dataAccess.abstracts.IAdminDao;
 import com.miro.dibt.entities.concretes.Admin;
 import lombok.AllArgsConstructor;
@@ -19,21 +22,24 @@ public class AdminManager implements IAdminService {
 
     @Override
     public DataResult<List<Admin>> getAll() {
-        return null;
+        return new SuccesDataResult(adminDao.findAll(), Messagess.adminListed);
     }
 
     @Override
     public IResult add(Admin admin) {
-        return null;
+        adminDao.save(admin);
+        return new SuccessResult(Messagess.adminSaved);
     }
 
     @Override
     public IResult update(Admin admin) {
-        return null;
+        adminDao.save(admin);
+        return new SuccessResult(Messagess.adminUpdate);
     }
 
     @Override
     public IResult delete(Admin admin) {
-        return null;
+        adminDao.delete(admin);
+        return new SuccessResult(Messagess.adminDelete);
     }
 }
