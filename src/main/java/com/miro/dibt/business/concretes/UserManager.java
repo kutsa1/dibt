e93e7dbad99482967c7f0d01e5312b724e043fcd
kutsa1 <82546005+kutsa1@users.noreply.github.com>
2021-12-05 15:40:29 +0300

    user manager is added

--- src/main/java/com/miro/dibt/business/concretes/UserManager.java
+++ src/main/java/com/miro/dibt/business/concretes/UserManager.java
@@ -1,8 +1,12 @@
 package com.miro.dibt.business.concretes;
 
 import com.miro.dibt.business.abstracts.IUserService;
+import com.miro.dibt.business.tools.Messagess;
 import com.miro.dibt.core.utilities.DataResult;
 import com.miro.dibt.core.utilities.IResult;
+import com.miro.dibt.core.utilities.SuccesDataResult;
+import com.miro.dibt.core.utilities.SuccessResult;
+import com.miro.dibt.dataAccess.abstracts.IUserDao;
 import com.miro.dibt.entities.concretes.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,23 +16,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserManager implements IUserService {
+    private final IUserDao iUserDao;
+
     @Override
     public DataResult<List<User>> getAll() {
-        return null;
+
+        return new SuccesDataResult(iUserDao.findAll(), Messagess.userListed);
     }
 
     @Override
     public IResult add(User user) {
-        return null;
+        iUserDao.save(user);
+        return new SuccessResult(Messagess.userSave);
     }
 
     @Override
     public IResult update(User user) {
-        return null;
+        iUserDao.save(user);
+        return new SuccessResult(Messagess.userUpdate);
     }
 
     @Override
     public IResult delete(User user) {
-        return null;
+        iUserDao.delete(user);
+        return new SuccessResult(Messagess.userDelete);
     }
 }
