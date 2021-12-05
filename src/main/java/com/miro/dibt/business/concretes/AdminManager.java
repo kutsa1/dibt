5f249c978d8cbc64cc841cd7b7ff3ac12f8ba669
kutsa1 <82546005+kutsa1@users.noreply.github.com>
2021-12-05 14:50:36 +0300

    changes on the dataAccess layers

--- src/main/java/com/miro/dibt/business/concretes/AdminManager.java
+++ src/main/java/com/miro/dibt/business/concretes/AdminManager.java
@@ -1,28 +1,39 @@
 package com.miro.dibt.business.concretes;
 
 import com.miro.dibt.business.abstracts.IAdminService;
+import com.miro.dibt.core.utilities.DataResult;
+import com.miro.dibt.core.utilities.IResult;
+import com.miro.dibt.dataAccess.abstracts.IAdminDao;
 import com.miro.dibt.entities.concretes.Admin;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+@Service
 public class AdminManager implements IAdminService {
+    private final IAdminDao adminDao;
+
+
     @Override
-    public List<Admin> getAll() {
+    public DataResult<List<Admin>> getAll() {
         return null;
     }
 
     @Override
-    public void add(Admin admin) {
-
+    public IResult add(Admin admin) {
+        return null;
     }
 
     @Override
-    public void update(Admin admin) {
-
+    public IResult update(Admin admin) {
+        return null;
     }
 
     @Override
-    public void delete(Admin admin) {
-
+    public IResult delete(Admin admin) {
+        return null;
     }
 }
