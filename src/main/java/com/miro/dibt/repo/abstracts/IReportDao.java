a2f2b0ee5eec6af85960c7cd4742dbfcd667817e
ismailkrc57 <ismailkrc57@gmail.com>
2021-12-12 19:37:16 +0300

    folder

--- src/main/java/com/miro/dibt/repo/abstracts/IReportDao.java
+++ src/main/java/com/miro/dibt/repo/abstracts/IReportDao.java
@@ -4,4 +4,5 @@ import com.miro.dibt.entities.concretes.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IReportDao extends JpaRepository<Report, Integer> {
+    Report getByIdAnd(int reportId);
 }
