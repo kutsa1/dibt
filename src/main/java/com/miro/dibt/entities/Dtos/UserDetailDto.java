43b09359eca6a36a35d13d1a5f0b137b45ff62ba
ismailkrc57 <ismailkrc57@gmail.com>
2022-01-02 14:34:37 +0300

    auth response edited

---
+++ src/main/java/com/miro/dibt/entities/Dtos/UserDetailDto.java
@@ -0,0 +1,13 @@
+package com.miro.dibt.entities.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailDto {
+    private int id;
+    private String username;
+}
